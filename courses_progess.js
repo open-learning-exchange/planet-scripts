0552bb3b740debd1101163635b31e944a8101956
Laxman Maharjan <email@mlaxman.com.np>
2020-08-25 12:45:32 +0545

    [#14] Courses Progress updatedDate fix

---
+++ courses_progess.js
@@ -0,0 +1,50 @@
+const request = require('request');
+
+const user = process.argv[2];
+const pass = process.argv[3];
+const domain = process.argv[4] || 'localhost:5984';
+const protocol = process.argv[5] || 'http';
+
+const uri = protocol + '://' + user + ':' + pass + '@' + domain + '/courses_progress/_all_docs?include_docs=true';
+const postUri = protocol + '://' + user + ':' + pass + '@' + domain + '/courses_progress';
+
+const batchSize = 500;
+
+const getCourseProgress = (callback) => {
+  request.get({ uri, json: true }, callback);
+}
+
+const postCallback = (progresses, index) => (err, response) => {
+  if (err) {
+    console.log(err);
+  }
+  postInBatches(progresses, index + batchSize);
+}
+
+const updateProgresses = (err, response) => {
+  const progresses = response.body.rows.map(({ doc }) => {
+    if (doc._id.indexOf('_design/') > -1) {
+      return;
+    }
+    if (doc.updatedDate) {
+      return;
+    }
+    return { ...doc, updatedDate: doc.createdOn, createdDate: doc.createdOn };
+  }).filter(progress => progress !== undefined);
+  postInBatches(progresses, 0);
+}
+
+const postInBatches = (progresses, startIndex) => {
+  if (startIndex > progresses.length) {
+    return;
+  }
+  console.log('Updating courses_progress ' + startIndex + ' through ' + (startIndex + batchSize - 1));
+  const progressBatch = progresses.slice(startIndex, startIndex + batchSize);
+  request.post({
+    uri: postUri + '/_bulk_docs',
+    body: { docs: progressBatch },
+    json: true
+  }, postCallback(progresses, startIndex));
+}
+
+getCourseProgress(updateProgresses);
